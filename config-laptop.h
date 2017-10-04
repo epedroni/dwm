f0242110a9e4b32921d3af68ad8cc9ea4158cfc0
Eduardo Pedroni <e.pedroni91@gmail.com>
2017-10-04 15:52:45 +0200

    Updated backlight controls

--- config-laptop.h
+++ config-laptop.h
@@ -119,8 +119,8 @@ static const char *lockcmd[]      = { "i3lock", "-c", "000000", NULL };
 static const char *lowervolumecmd[]  = { "amixer", "-q", "set", "Master", "2dB-", NULL };
 static const char *raisevolumecmd[]  = { "amixer", "-q", "set", "Master", "2dB+", NULL };
 static const char *mutevolumecmd[]   = { "amixer", "-q", "set", "Master", "toggle", NULL };
-static const char *raisebrightness[] = { "xbacklight", "-inc", "5", NULL };
-static const char *lowerbrightness[] = { "xbacklight", "-dec", "5", NULL };
+static const char *raisebrightness[] = { "xbacklight", "-set", "1", NULL };
+static const char *lowerbrightness[] = { "xbacklight", "-set", "20", NULL };
 static const char *toggletrackpad[]  = { "toggle-trackpad", NULL };
 
 
